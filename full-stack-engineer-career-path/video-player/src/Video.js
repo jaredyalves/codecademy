35c2963d01888bfa243f10d9c1704b1aa677db0c
Járedy Alves <94395541+jaredyalves@users.noreply.github.com>
2023-11-10 15:05:21 +0000

    Add video-player project

---
+++ full-stack-engineer-career-path/video-player/src/Video.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Video extends React.Component {
+  render() {
+    return (<div>
+      <video src={this.props.src} controls autostart autoPlay muted/>
+    </div>);
+  }
+}
